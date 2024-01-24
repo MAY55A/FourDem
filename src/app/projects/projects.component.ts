3ae74b89637b5c7c3cad50412c3f286cab0c13d3
mayssaGH <mayssagnm@gmail.com>
2024-01-24 16:09:59 +0100

    implémentation de l'interface 'projets proposés'

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css','../profile/my-projects/my-projects.component.css']
 })
 export class ProjectsComponent {
 
