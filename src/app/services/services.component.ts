595f5075deb69030b56f614d814c6cb67c915284
mayssaGH <mayssagnm@gmail.com>
2024-02-14 11:29:29 +0100

    modification du composant services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ServiceService } from '../_services/service.service';
+import { Service } from '../_models/service';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.css']
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit{
 
+  constructor(private route: ActivatedRoute, private service: ServiceService, private router: Router) {}
+
+  domain!: string | null;
+  allServices!: Service[];
+  services!: Service[];
+  searchValue = '';
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      (params: ParamMap)=> {
+        this.domain = params.get('domaine');
+        if(!this.domain)
+          this.getAllServices();
+        else
+          this.getServicesByDomain(this.domain);
+        });
+  }
+
+  getAllServices() {
+    this.service.getAllServices().subscribe(
+      (services) => {
+        this.allServices = services;
+        this.services = services;
+      }
+    );
+  }
+  getServicesByDomain(d: string) {
+    this.service.getServicesByDomain(d).subscribe(
+      (services) => {
+        this.allServices = services;
+        this.services = services;
+      }
+    );
+  }
+  changeDomain() {
+    this.router.navigate(['../services/',this.domain])
+  }
+
+  search() {
+    this.services = this.allServices?.filter(s => s.proposerName.toLowerCase().includes(this.searchValue) || s.description.toLowerCase().includes(this.searchValue) || s.skills.toLowerCase().includes(this.searchValue));
+  }
+  
+  replace(s: string): string {
+    return s.replace(/-/g, ' ');
+  }
 }
