9ca04ad4d9f3482214610aadf4d9d763e25815f6
mayssaGH <mayssagnm@gmail.com>
2024-03-24 13:00:04 +0100

    modification des composant my-projects et my-services

--- src/app/profile/my-projects/my-projects.component.ts
+++ src/app/profile/my-projects/my-projects.component.ts
@@ -5,6 +5,8 @@ import { OptionsService } from 'src/app/_services/options.service';
 import { ProjectService } from 'src/app/_services/project.service';
 import { ProfileComponent } from '../profile.component';
 import { AppComponent } from 'src/app/app.component';
+import { ProfileService } from 'src/app/_services/profile.service';
+import { User } from 'src/app/_models/user';
 
 
 @Component({
@@ -14,32 +16,36 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class MyProjectsComponent implements OnInit {
 
-  constructor( private profile: ProfileComponent, public app: AppComponent, private projectService: ProjectService, private route: ActivatedRoute, public options: OptionsService) { }
-  user = this.profile.user;
+  constructor(private profileService: ProfileService, public app: AppComponent, private projectService: ProjectService, private route: ActivatedRoute, public options: OptionsService) { }
+  user!: User;
   status!: string | null;
   allProjects!: Project[];
   projects: Project[] = [];
   searchValue = '';
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      (params: ParamMap) => {
-        this.status = params.get("status");
-        this.getProjectsByStatus();
-      }
-    )
+    this.profileService.user$.subscribe((user) => {
+      this.user = user!;
+      this.route.paramMap.subscribe(
+        (params: ParamMap) => {
+          this.status = params.get("status");
+          this.getProjectsByStatus();
+        }
+      )
+    },
+    (err) => {
+      console.log(err);
+    }
+    );
+
   }
 
   getProjectsByStatus() {
-    this.projectService.getProjectsByUser(this.user!.id!).subscribe(
-      (projects) => {
-        this.allProjects = projects.filter(p => p.status == this.status);
-        this.projects = [...this.allProjects];
-      }
-    )
+    this.allProjects = this.user!.projects!.filter(p => p.status == this.status);
+    this.projects = [...this.allProjects];
   }
 
   search() {
-    this.projects = this.allProjects?.filter(p => p.title.includes(this.searchValue) || p.categories.includes(this.searchValue));
+    this.projects = this.allProjects?.filter(p => p.title.toLowerCase().includes(this.searchValue) || p.categories.map(c=>c.name.toLowerCase()).some(c => c.includes(this.searchValue)));
   }
 }
--- src/app/profile/my-services/my-services.component.ts
+++ src/app/profile/my-services/my-services.component.ts
@@ -1,45 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from 'src/app/_services/service.service';
-import { ProfileComponent } from '../profile.component';
 import { Service } from 'src/app/_models/service';
 import { AppComponent } from 'src/app/app.component';
+import { ProfileService } from 'src/app/_services/profile.service';
+import { User } from 'src/app/_models/user';
 
 @Component({
   selector: 'app-my-services',
   templateUrl: './my-services.component.html',
-  styleUrls: ['./my-services.component.css']
+  styleUrls: ['./my-services.component.css', '../../services/services.component.css']
 })
 export class MyServicesComponent implements OnInit{
 
-  constructor(private service: ServiceService, private profile: ProfileComponent, public app: AppComponent) {}
+  constructor(private service: ServiceService, private profileService: ProfileService, public app: AppComponent) {}
 
-  user = this.profile.user;
-  allServices: any[] = [];
-  services: any[]= [];
+  user!: User;
+  allServices: Service[] = [];
+  services: Service[]= [];
   searchValue = '';
 
   ngOnInit(): void {
-    this.getServices();
+    this.profileService.user$.subscribe((user) => {
+      this.user = user!;
+      this.allServices = user!.services!;
+      this.services = user!.services!;
+    });
   }
 
-  getServices() {
-    this.service.getServicesByUser(this.user.id!).subscribe(
-      (services) => {
-        this.allServices = services;
-        this.services = services;
-      }
-    )
-  }
-
-  deleteservice(id: number) {
-    this.service.deleteService(id).subscribe(
-      () => {
-        console.log("deleting service...");
-      }
-    )
+  deleteservice(s: Service) {
+    if (confirm('Voulez vous vraiment supprimer ce service ?')) {
+      this.service.deleteServiceAndNotify(s).subscribe(
+        () => {
+          console.log("deleting service...");
+          window.location.reload();
+        }
+      )
+    }
   }
 
   search() {
-    this.services = this.allServices?.filter(s => s.description.includes(this.searchValue) || s.skills.includes(this.searchValue));
+    this.services = this.allServices?.filter(s => s.description.includes(this.searchValue) || s.skills.toLowerCase().includes(this.searchValue));
+  }
+
+  replace(s: string): string {
+    return s.replace(/-/g, ' ');
   }
 }
